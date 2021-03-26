63bf4e7047c1fe856eb9c1c41a382de2c6c29167
garjania <>
2021-03-26 17:20:58 +0430

    Fixed assertEquals inputs in testArea and passed test

--- test/TestShape.java
+++ test/TestShape.java
@@ -6,6 +6,6 @@ public class TestShape {
     @Test
     public void testArea() {
         Rectangle rect = new Rectangle(3.1, 4);
-        assertEquals(rect.computeArea(), 12.4);
+        assertEquals(rect.computeArea(), 12.4, 0.001);
     }
 }
