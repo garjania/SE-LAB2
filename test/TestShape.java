baf36094b0afafe022d59fb44f5e651690e6d8d0
garjania <>
2021-03-26 17:13:14 +0430

    Add testArea to TestShape script

---
+++ test/TestShape.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestShape {
+
+    @Test
+    public void testArea() {
+        Rectangle rect = new Rectangle(3.1, 4);
+        assertEquals(rect.computeArea(), 12.4);
+    }
+}
