d7ae1508f29a05aefcf8b5e282d252359d4ed9c6
garjania <>
2021-03-26 17:18:42 +0430

    Create Rectangle class and added computeArea method

---
+++ src/Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle {
+
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        if(width > 0)
+            this.width = width;
+        if(height > 0)
+            this.height = height;
+    }
+
+    public double computeArea() {
+        return width * height;
+    }
+
+}
